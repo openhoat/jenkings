f6367b24ff6444b0d083e2104b31c0abf328d906
Olivier Penhoat <openhoat@gmail.com>
2014-04-14 10:03:56 +0200

    ui enhancement

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -43,7 +43,7 @@ angular.module('app', [
       prefix: '/languages/',
       suffix: '.json'
     });
-    $translateProvider.preferredLanguage('fr');
+    //$translateProvider.preferredLanguage('fr');
   })
   .run(function ($rootScope, $location, $localStorage, $translate) {
     $rootScope.jenkinsBaseUrl = $localStorage.jenkinsBaseUrl || 'http://ci.myserver.com/';
